4fc3652b3f7365052c1c608d99e553bff645b264
Lindsay Neubauer <neubauer.lindsay@gmail.com>
2014-12-07 00:44:34 -0500

    added 'waiting' message for long API calls

--- js/person.js
+++ js/person.js
@@ -1,10 +1,10 @@
 // API keys
 var auth = {
   version: "v3",
-  campaign_finance_api_key: "693917afeaba58fde1552a6732852e9c:18:70213515",
-  congress_api_key: "eab06b972a38a20d0e58eb3d8cdc7c58:7:70213515",
-  times_newswire_api_key: "79e51b60c583682981c77388db8f55d9:10:70213515",
-  article_search_api_key: "9e9d9f28b5f5b62d79968c128fdf7a66:9:70213515",
+  campaign_finance_api_key: "",
+  congress_api_key: "",
+  times_newswire_api_key: "",
+  article_search_api_key: ""
 }
 
 var members = [];
@@ -326,6 +326,7 @@ function getMemberBio() {
     url = "http://api.nytimes.com/svc/politics/" + auth.version + "/us/legislative/congress/members/" + chamber + "/" + state + "/current.json?api-key=" + auth.congress_api_key;
   }
 
+  $("#bio_name").append("Retrieving information from NYT API...");
   $.ajax({
     url: url,
 		type: "get",
@@ -333,6 +334,7 @@ function getMemberBio() {
 		cache: true,
     success: function(data) {
       console.log(data);
+      $("#bio_name").empty();
 
       $.each(data["results"], function(i, result) {
         var full_name = result["name"].split(' ');
